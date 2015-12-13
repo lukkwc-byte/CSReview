27a57217ba5f98b66a7bab081463bf19a674fff4
nikelaren <luk.kwc@gmail.com>
2015-12-13 16:07:18 -0500

    Finished Tree implementation and its traversals

--- Trees.py
+++ Trees.py
@@ -1,11 +1,41 @@
 class TreeNode():
     def __init__(self, data=None, left=None, right=None):
-        self.data=None
-        self.left=None
-        self.right=None
+        self.data=data
+        self.left=left
+        self.right=right
 
-    def __str___(self):
+    def __str__(self):
         return str(self.data)
 
+
 class Tree():
-    self
+    def __init__(self, root=None):
+        self.root=root
+
+    def preorder(self, node):
+        print(node)
+        if node.left:
+            Tree.preorder(self, node.left)
+        if node.right:
+            Tree.preorder(self, node.right)
+
+    def inorder(self, node):
+        if node.left: Tree.inorder(self, node.left)
+        print(node)
+        if node.right: Tree.inorder(self, node.right)
+
+    def postorder(self, node):
+        if node.left: Tree.postorder(self, node.left)
+        if node.right: Tree.postorder(self, node.right)
+        print(node)
+
+
+d=TreeNode("D")
+e=TreeNode("E")
+f=TreeNode("F")
+g=TreeNode("G")
+b=TreeNode("B", d, e)
+c=TreeNode("C", f, g)
+a=TreeNode("A", b, c)
+test=Tree(a)
+test.inorder(test.root)
